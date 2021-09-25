a16050b5873781e4141f0b08d83fa7b6d09d263d
ailict <ailichatfieldtaylor@gmail.com>
2021-09-24 22:04:12 -0400

    added questions to javascript array

---
+++ script.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+    question: "Commonly used data types do NOT include:",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts"
+  }, {
+    question: "The condition of an if/else statement is enclosed with _____.",
+    options: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+    answer: "3. parentheses" 
+  }, {
+    question: "Arrays in Javascript can be used to store _____.",
+    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4. all of the above"
+  }, {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
+    answer: "3. quotes" 
+  }, {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    answer: "4. console.log"
+  }]
