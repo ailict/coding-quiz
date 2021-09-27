185920a8cc8a871b73deecc8bee8c94766e1d84e
ailict <ailichatfieldtaylor@gmail.com>
2021-09-26 20:01:22 -0400

    updated formatting of html and js sheets

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-//variable setup
+// variable setup
 var viewHighscoresAnchor = document.getElementById("view-highscores");
 
-var timer = document.querySelector(".time");
+var timerCounter = document.getElementById("timerCounter");
 
 document.getElementById("intro").style.display = "block";
 var intro = document.getElementById("intro");
@@ -26,35 +26,49 @@ var submitInitialsButton = document.getElementById("submit-initials-button");
 
 var questionsArray = [
     {
-    question: "Commonly used data types do NOT include:",
-    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
-    answer: "3. alerts"
-  }, {
-    question: "The condition of an if/else statement is enclosed with _____.",
-    options: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
-    answer: "3. parentheses" 
-  }, {
-    question: "Arrays in Javascript can be used to store _____.",
-    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
-    answer: "4. all of the above"
-  }, {
-    question: "String values must be enclosed within _____ when being assigned to variables.",
-    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"],
-    answer: "3. quotes" 
-  }, {
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
-    answer: "4. console.log"
-  }];
+        question: "Commonly used data types do NOT include:",
+        options: [
+            "1. strings", "2. booleans", "3. alerts", "4. numbers"
+        ],
+        answer: "3. alerts"
+    },
+    {
+        question: "The condition of an if/else statement is enclosed with _____.",
+        options: [
+            "1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"
+        ],
+        answer: "3. parentheses"
+    },
+    {
+        question: "Arrays in Javascript can be used to store _____.",
+        options: [
+            "1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"
+        ],
+        answer: "4. all of the above"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        options: [
+            "1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"
+        ],
+        answer: "3. quotes"
+    }, {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: [
+            "1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"
+        ],
+        answer: "4. console.log"
+    }
+];
 
 function startQuiz() {
     document.getElementById("intro").style.display = "none";
-    document.getElementById("quiz").style.display = "block";    
+    document.getElementById("quiz").style.display = "block";
     populateQuestion();
-  };
+};
 
 var correctAnswer;
-var num=0;
+var num = 0;
 function populateQuestion() {
     currentQuestion.textContent = questionsArray[num].question;
     option1.textContent = questionsArray[num].options[0];
@@ -62,21 +76,20 @@ function populateQuestion() {
     option3.textContent = questionsArray[num].options[2];
     option4.textContent = questionsArray[num].options[3];
     correctAnswer = questionsArray[num].answer;
-  };
+};
 
-  quiz.addEventListener("click", function (event) {
+quiz.addEventListener("click", function (event) {
     var element = event.target;
-        if (element = questionsArray[num].answer){
-      num++;
-      if (num < questionsArray.length) {
-        populateQuestion();
-      } else {
-        isDone = true;
-      }
+    if (element = questionsArray[num].answer) {
+        num++;
+        if (num < questionsArray.length) {
+            populateQuestion();
+        } else {
+            isDone = true;
+        }
     } else {
-      timerCount = timerCount - 10;
+        timerCount = timerCount - 10;
     }
-  });
+});
 
- startButton.addEventListener("click", startQuiz)
-
+startButton.addEventListener("click", startQuiz)
